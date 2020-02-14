a64e8922b35bc14ff3888f89522421038be12534
Ashley M Mader <ashley.mader12@gmail.com>
2020-02-14 21:11:33 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,9 +9,10 @@ end
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 # code your input_to_index and move method here!
+user input = ["1","2", "3", "4", "5", "6", "7", "8", "9"]
 
 def input_to_index(user_input)
-  puts (user_input) -1 
+  puts (user_input) 
 end 
 
 def move
